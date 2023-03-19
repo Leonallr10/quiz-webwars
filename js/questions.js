10e0e4da6e239cac85b1ad9432a72e88879f6ea0
Leonallr10 <118210551+Leonallr10@users.noreply.github.com>
2023-03-19 19:37:44 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,186 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The correct sequence of HTML tags for starting a webpage is -",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+      "HTML, Head,  Body, Title",
+      "HTML, Head, Title, Body"
+    ]
+  },
+
+    {
+    numb: 7,
+    question: "Which of the following tag is used for inserting the largest heading in HTML?",
+    answer: "h1",
+    options: [
+      "h3",
+      "h1",
+      "h5",
+      "h6"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the capital of France?",
+    answer: "Paris",
+    options: [
+      "Madrid",
+      "Paris",
+      "Rome",
+      "Berlin"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the largest country in the world?",
+    answer: "Russia",
+    options: [
+      "United States",
+      "China",
+      "Russia",
+      "Canada"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the currency of Japan?",
+    answer: "Yen",
+    options: [
+      "Dollar",
+      "Pound",
+      "Euro",
+      "Yen"
+    ]
+  },
+
+{
+  numb: 11,
+
+question: "What is the purpose of HTML?",
+answer: "To structure content and provide a framework for webpages",
+options: [
+"To style webpages with CSS",
+"To create interactive functionality with JavaScript",
+"To structure content and provide a framework for webpages",
+"To manage server-side operations"
+]
+},
+{
+numb: 12,
+question: "What is JavaScript primarily used for in web development?",
+answer: "To add interactivity and dynamic functionality to webpages",
+options: [
+"To structure content on webpages",
+"To design and style webpages with CSS",
+"To add interactivity and dynamic functionality to webpages",
+"To manage server-side operations"
+]
+},
+{
+numb: 13,
+question: "What is the difference between inline and external JavaScript?",
+answer: "Inline JavaScript is included directly in HTML code, while external JavaScript is stored in a separate file and linked to HTML code.",
+options: [
+"Inline JavaScript is used for client-side operations, while external JavaScript is used for server-side operations.",
+"Inline JavaScript is used for server-side operations, while external JavaScript is used for client-side operations.",
+"Inline JavaScript is included directly in HTML code, while external JavaScript is stored in a separate file and linked to HTML code.",
+"There is no difference between inline and external JavaScript."
+]
+},
+{
+numb: 14,
+question: "What is the purpose of the <head> tag in HTML?",
+answer: "To contain metadata about the HTML document",
+options: [
+"To display the content of the web page",
+"To contain the main content of the web page",
+"To define the structure of the web page",
+"To contain metadata about the HTML document"
+]
+},
+
+{
+numb: 15,
+question: "What is an example of a JavaScript data type?",
+answer: "String",
+options: [
+"CSS",
+"HTML",
+"jQuery",
+"String"
+]
+},
+
+{
+numb: 16,
+question: "What is the purpose of the 'getElementById' method in JavaScript?",
+answer: "To retrieve an element from an HTML document based on its ID",
+options: [
+"To retrieve all elements from an HTML document",
+"To create a new element in an HTML document",
+"To retrieve an element from an HTML document based on its class name",
+"To retrieve an element from an HTML document based on its ID"
+]
+},
+
+
+]
